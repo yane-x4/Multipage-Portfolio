e8b0732ddb3b6c3b2d1d2af6b728d71d709cbf9c
yane-x4 <yanesamedina9@gmail.com>
2026-04-29 21:52:58 +0800

    Add files via upload

---
+++ pages/⚒️_Skills.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("🛠️ SKILLS")
+
+st.subheader("🖥️ Programming")
+st.progress(80)
+st.write("Python")
+
+st.progress(50)
+st.write("HTML")
+
+st.progress(65)
+st.write("CSS")
+
+st.subheader("🎨 Design")
+st.progress(80)
+st.write("- Canva/ UI Designs")
+
+st.subheader("⚒️ TOOLS")
+st.write("- Streamlit")
+st.write("- GitHub")
+st.write("- Visual Studio Code")
